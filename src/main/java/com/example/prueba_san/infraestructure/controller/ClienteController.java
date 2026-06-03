d764614c52df8cb3c5fdb3506e006aef056fe64d
yeprepue <yepretel@misena.edu.co>
2026-06-03 11:14:23 -0500

    Commit inicial infraestructura y archivos

---
+++ src/main/java/com/example/prueba_san/infraestructure/controller/ClienteController.java
@@ -0,0 +1,5 @@
+package com.example.prueba_san.infraestructure.controller;
+
+public class ClienteController {
+    
+}
---
+++ src/main/java/com/example/prueba_san/infraestructure/exception/GlobalExeptionHandler.java
@@ -0,0 +1,5 @@
+package com.example.prueba_san.infraestructure.exception;
+
+public class GlobalExeptionHandler {
+    
+}
